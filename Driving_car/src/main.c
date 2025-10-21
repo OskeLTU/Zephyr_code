ba3762c4b3c6737612622436d33e862879468af4
Oske <oskepower@gmail.com>
2025-10-21 15:29:27 +0200

    started implimantion of pwm

--- Driving_car/src/main.c
+++ Driving_car/src/main.c
@@ -1,16 +1,16 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/pwm.h>
 
-static const struct gpio_dt_spec my_led = GPIO_DT_SPEC_GET(DT_NODELABEL(external_led_0), gpios);
+static const struct gpio_dt_spec mototr_a_in1 = GPIO_DT_SPEC_GET(DT_NODELABEL(mototr_a_in1), gpios);
+static const struct gpio_dt_spec mototr_a_in2 = GPIO_DT_SPEC_GET(DT_NODELABEL(mototr_a_in2), gpios);
+static const struct gpio_dt_spec mototr_b_in1 = GPIO_DT_SPEC_GET(DT_NODELABEL(mototr_b_in1), gpios);
+static const struct gpio_dt_spec mototr_b_in2 = GPIO_DT_SPEC_GET(DT_NODELABEL(mototr_b_in2), gpios);
+
+static const struct pwm_dt_spec motor_speed_a = DEVICE_DT_GET(DT_NODELABEL(motor_speed_a));
+static const struct pwm_dt_spec motor_speed_b = DEVICE_DT_GET(DT_NODELABEL(motor_speed_b));
 
 int main(void)
 {
-    gpio_pin_configure_dt(&my_led, GPIO_OUTPUT_ACTIVE);
-
-    while (1) {
-        gpio_pin_set_dt(&my_led, 1);
-        k_msleep(500);
-        gpio_pin_set_dt(&my_led, 0);
-        k_msleep(500);
-    }
+    
 }
