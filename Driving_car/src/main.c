f3d7d5dc7bac12b37080d947279fede39ed9d856
Oske <oskepower@gmail.com>
2025-10-22 15:55:06 +0200

    continue

--- Driving_car/src/main.c
+++ Driving_car/src/main.c
@@ -2,13 +2,15 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/pwm.h>
 
-static const struct gpio_dt_spec mototr_a_in1 = GPIO_DT_SPEC_GET(DT_NODELABEL(mototr_a_in1), gpios);
-static const struct gpio_dt_spec mototr_a_in2 = GPIO_DT_SPEC_GET(DT_NODELABEL(mototr_a_in2), gpios);
-static const struct gpio_dt_spec mototr_b_in1 = GPIO_DT_SPEC_GET(DT_NODELABEL(mototr_b_in1), gpios);
-static const struct gpio_dt_spec mototr_b_in2 = GPIO_DT_SPEC_GET(DT_NODELABEL(mototr_b_in2), gpios);
+static const struct gpio_dt_spec motor_a_in1 = GPIO_DT_SPEC_GET(DT_NODELABEL(motor_a_in1), gpios);
+static const struct gpio_dt_spec motor_a_in2 = GPIO_DT_SPEC_GET(DT_NODELABEL(motor_a_in2), gpios);
+static const struct gpio_dt_spec motor_b_in1 = GPIO_DT_SPEC_GET(DT_NODELABEL(motor_b_in1), gpios);
+static const struct gpio_dt_spec motor_b_in2 = GPIO_DT_SPEC_GET(DT_NODELABEL(motor_b_in2), gpios);
 
-static const struct pwm_dt_spec motor_speed_a = DEVICE_DT_GET(DT_NODELABEL(motor_speed_a));
-static const struct pwm_dt_spec motor_speed_b = DEVICE_DT_GET(DT_NODELABEL(motor_speed_b));
+
+
+static const struct pwm_dt_spec motor_a_speed = PWM_DT_SPEC_GET(DT_NODELABEL(motor_a_speed));
+static const struct pwm_dt_spec motor_b_speed = PWM_DT_SPEC_GET(DT_NODELABEL(motor_b_speed));
 
 int main(void)
 {
