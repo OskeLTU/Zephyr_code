d661447992336d8b0d51042429d9754c993e8e18
Oske <oskepower@gmail.com>
2025-10-23 10:10:19 +0200

    Updated for simple testing

--- Driving_car/src/main.c
+++ Driving_car/src/main.c
@@ -9,13 +9,47 @@ static const struct gpio_dt_spec motor_b_in2 = GPIO_DT_SPEC_GET(DT_NODELABEL(mot
 
 
 
-static const struct pwm_dt_spec motor_a_speed = PWM_DT_SPEC_GET(DT_NODELABEL(motor_a_speed));
-static const struct pwm_dt_spec motor_b_speed = PWM_DT_SPEC_GET(DT_NODELABEL(motor_b_speed));
-
+// static const struct pwm_dt_spec motor_a_speed = PWM_DT_SPEC_GET(DT_NODELABEL(motor_a_speed));
+// static const struct pwm_dt_spec motor_b_speed = PWM_DT_SPEC_GET(DT_NODELABEL(motor_b_speed));
 
 
 
 int main(void)
 {
+    /* --- 2. CHECK IF ALL DEVICES ARE READY --- */
+    if (!gpio_is_ready_dt(&motor_a_in1) || !gpio_is_ready_dt(&motor_a_in2) ||
+        !gpio_is_ready_dt(&motor_b_in1) || !gpio_is_ready_dt(&motor_b_in2)){
+        return 0; // Pins not ready
+    }
 
+    /* --- 3. CONFIGURE ALL 6 PINS AS OUTPUTS --- */
+    gpio_pin_configure_dt(&motor_a_in1, GPIO_OUTPUT_INACTIVE);
+    gpio_pin_configure_dt(&motor_a_in2, GPIO_OUTPUT_INACTIVE);
+    gpio_pin_configure_dt(&motor_b_in1, GPIO_OUTPUT_INACTIVE);
+    gpio_pin_configure_dt(&motor_b_in2, GPIO_OUTPUT_INACTIVE);
+    
+
+
+    /* --- 4. CONTROL LOOP --- */
+    while (1) {
+        // --- Motors forward at 100% speed ---
+        gpio_pin_set_dt(&motor_a_in1, 1); // Motor A Forward (IN1=HIGH, IN2=LOW)
+        gpio_pin_set_dt(&motor_a_in2, 0);
+        
+        gpio_pin_set_dt(&motor_b_in1, 1); // Motor B Forward (IN3=HIGH, IN4=LOW)
+        gpio_pin_set_dt(&motor_b_in2, 0);
+
+        k_msleep(3000); // Run forward for 3 seconds
+
+        // --- Motors stop (coast) ---
+        /* Turn off Enable pins */
+        gpio_pin_set_dt(&motor_a_in1, 0); // Motor A Forward (IN1=HIGH, IN2=LOW)
+        gpio_pin_set_dt(&motor_a_in2, 1);
+        
+        gpio_pin_set_dt(&motor_b_in1, 0); // Motor B Forward (IN3=HIGH, IN4=LOW)
+        gpio_pin_set_dt(&motor_b_in2, 1);
+
+        k_msleep(3000); // Stop for 3 seconds
+    }
+    return 0;
 }
