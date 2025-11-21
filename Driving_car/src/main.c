c18328578640da164a25d57642560c47945c59da
Oske <oskepower@gmail.com>
2025-11-21 12:49:52 +0100

    g

--- Driving_car/src/main.c
+++ Driving_car/src/main.c
@@ -24,12 +24,30 @@ int main(void){
     // k_msleep(5000);
     // Stop_motors();
     // }
+    while(1){
+       Drive_motors(Forward, 100);
+       k_msleep(2000);
+         Stop_motors();
+         k_msleep(2000);
+         Drive_motors(Backward, 100);
+            k_msleep(2000);
+            Stop_motors();
+            k_msleep(2000);
+            Drive_motors(Forward, 50);
+            k_msleep(2000);
+            Stop_motors();
+            k_msleep(2000);
+            Drive_motors(Backward, 50);
+               k_msleep(2000);
+                Stop_motors();
+                k_msleep(2000);
+    }
 
-    Drive_one_motor(Motor_A_Front, Forward, 100);
-    Drive_one_motor(Motor_B_Front, Forward, 100);
+    Drive_one_motor(Motor_A_Front, Backward, 100);
+    Drive_one_motor(Motor_B_Front, Backward, 100);
 
-    // Drive_one_motor(Motor_A_Back, Forward, 100);
-    // Drive_one_motor(Motor_B_Back, Forward, 100);
+    Drive_one_motor(Motor_A_Back, Backward, 100);
+    Drive_one_motor(Motor_B_Back, Backward, 100);
 
     
     return 0;
