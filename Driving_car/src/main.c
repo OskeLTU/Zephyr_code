8f7f33e02ebcb8b780fb2f3f5ec7be1db71aa6ed
Oske <oskepower@gmail.com>
2025-11-30 17:35:03 +0100

    I2c first try

--- Driving_car/src/main.c
+++ Driving_car/src/main.c
@@ -6,16 +6,43 @@
 #include "pwm.h"
 #include "motor_controls.h"
 
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/i2c.h>
 
+/* Get the I2C device pointer */
+static const struct device *const i2c_dev = DEVICE_DT_GET(DT_NODELABEL(i2c1));
 
-int main(void){
+#define TARGET_ADDR 0x42
 
-    pwm_begin();
-    motors_start();
+int main(void)
+{
+    printk("--- I2C Sender Starting ---\n");
 
-    Drive_motors(Forward, 50);
+    /* Check if the device driver was initialized */
+    if (!device_is_ready(i2c_dev)) {
+        printk("Error: I2C device not ready.\n");
+        return 0;
+    }
 
-    
+    uint8_t data = 0;
+
+    while (1) {
+        printk("Sending data: %d... ", data);
+
+        /* Master write to target */
+        int ret = i2c_write(i2c_dev, &data, 1, TARGET_ADDR);
+
+        if (ret == 0) {
+            printk("ACK received!\n");
+        } else {
+            printk("Error %d (No ACK?)\n", ret);
+        }
+
+        data++;
+        k_msleep(2000);
+    }
     return 0;
 }
-
